166978fee86c8a41307e40970c48e6a2d1ec7180
natee <samaneranateechai@gmail.com>
2022-11-30 08:27:54 +0700

    update Date input:

--- src/pages/admin/createNew/CreateNew.tsx
+++ src/pages/admin/createNew/CreateNew.tsx
@@ -1,18 +1,62 @@
-import React from 'react'
-import { FormControl, FormCreate } from './create.styled'
-import axios from 'axios'
+import React, { ReactElement } from "react";
+import { FormControl, FormCreate, FormRow, Container } from "./create.styled";
+import axios from "axios";
 
-type Props = {}
+type Props = {};
+
+const gennerateMonthTh = (days: number) => {
+  const months_th = [
+    "มกราคม",
+    "กุมภาพันธ์",
+    "มีนาคม",
+    "เมษายน",
+    "พฤษภาคม",
+    "มิถุนายน",
+    "กรกฎาคม",
+    "สิงหาคม",
+    "กันยายน",
+    "ตุลาคม",
+    "พฤศจิกายน",
+    "ธันวาคม",
+  ];
+  const months_th_mini = [
+    "ม.ค.",
+    "ก.พ.",
+    "มี.ค.",
+    "เม.ย.",
+    "พ.ค.",
+    "มิ.ย.",
+    "ก.ค.",
+    "ส.ค.",
+    "ก.ย.",
+    "ต.ค.",
+    "พ.ย.",
+    "ธ.ค.",
+  ];
+
+  var month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+};
 
 function CreateNew({}: Props) {
-
-  const insertAPI = (e:any) => {
-    e.preventDefault()
+  const insertAPI = (e: any) => {
+    e.preventDefault();
     // e.target.time.value = '1669730603239'
     const valueDate = new Date(`Sat Nov 08 2022 17:30:00`);
 
-    console.log(valueDate)
-
+    console.log(valueDate);
 
     // axios({
     //   method: "post",
@@ -22,71 +66,90 @@ function CreateNew({}: Props) {
 
     //     },
     //     addressData: {
-          
+
     //     },
     //     workData: {
 
     //     }
     //   }
     // })
-  } 
+  };
   return (
     <div>
-      <FormCreate onSubmit={insertAPI}>
-        <FormControl>
-          <label>ชื่อ</label>
-          <input name='firstNamee' type="text" placeholder='ชื่อ' />
-        </FormControl>
-        <FormControl>
-          <label>สกุล</label>
-          <input name='lastName' type="text" placeholder='สกุล' />
-        </FormControl>
+      <Container>
+        <FormCreate onSubmit={insertAPI}>
+          <FormControl>
+            <label>ชื่อ</label>
+            <input name="firstNamee" type="text" placeholder="ชื่อ" />
+          </FormControl>
+          <FormControl>
+            <label>สกุล</label>
+            <input name="lastName" type="text" placeholder="สกุล" />
+          </FormControl>
 
-        <FormControl>
-          <label>ติดต่อ</label>
-          <input name='tell' type="text" placeholder='เบอร์' />
-        </FormControl>
+          <FormControl>
+            <label>ติดต่อ</label>
+            <input name="tell" type="text" placeholder="เบอร์" />
+          </FormControl>
 
-        <FormControl>
-          <label>เนื่องในงาน</label>
-          <input name='workType' type="text" placeholder='เนื่องในงาน' />
-        </FormControl>
+          <FormControl>
+            <label>เนื่องในงาน</label>
+            <input name="workType" type="text" placeholder="เนื่องในงาน" />
+          </FormControl>
 
-        <FormControl>
-          <label>เวลา</label>
-          <input  name='time' type="time" placeholder='เวลา' />
-        </FormControl>
+          <FormControl>
+            <label>เวลา</label>
+            <input name="time" type="time" placeholder="เวลา" />
+          </FormControl>
 
-        <FormControl>
-          <label>วันที่</label>
-          <input type="number" min={1} max={31} placeholder='วันที่' />
-          <select name="month" id="">
-            <option value="jen">มกราคม</option>
-          </select>
-        </FormControl>
+          <FormRow>
+            <FormControl>
+              <label>ปี</label>
+              <select name="years" id="">
+                <option value={new Date(Date.now()).getFullYear() + 543}>
+                  {new Date(Date.now()).getFullYear() + 543}
+                </option>
+                <option value={new Date(Date.now()).getFullYear() + 544}>
+                  {new Date(Date.now()).getFullYear() + 544}
+                </option>
+              </select>
+            </FormControl>
+            <FormControl>
+              <label>เดือน</label>
+              <select name="month" id="">
+                <option value="jen">มกราคม</option>
+              </select>
+            </FormControl>
+            <FormControl>
+              <label>วันที่</label>
+              <input type="number" min={1} max={31} placeholder="วันที่" />
+            </FormControl>
+          </FormRow>
 
-        <FormControl>
-          <label>สถานที่</label>
-          <select name="location" id="">
-            <option value="วิหาร">วิหาร</option>
-            <option value="ศาลา">ศาลา</option>
-          </select>
-        </FormControl>
+          <FormControl>
+            <label>สถานที่</label>
+            <select name="location" id="location">
+              <option value="วิหาร">วิหาร</option>
+              <option value="ศาลา">ศาลา</option>
+              <option value="ข้างนอก">ข้างนอก</option>
+            </select>
+          </FormControl>
 
-        <FormControl>
-          <label>พระ</label>
-          <input type="number" max={10} min={1} placeholder='พระ' />
-        </FormControl>
+          <FormControl>
+            <label>พระ</label>
+            <input type="number" max={10} min={1} placeholder="พระ" />
+          </FormControl>
 
-        <FormControl>
-          <label>หมายเหตุ</label>
-          <input name='detail' type="text" placeholder='หมายเหตุ' />
-        </FormControl>
+          <FormControl>
+            <label>หมายเหตุ</label>
+            <input name="detail" type="text" placeholder="หมายเหตุ" />
+          </FormControl>
 
-        <button>ตกลง</button>
-      </FormCreate>
+          <button>ตกลง</button>
+        </FormCreate>
+      </Container>
     </div>
-  )
+  );
 }
 
-export default CreateNew
+export default CreateNew;
--- src/pages/admin/createNew/create.styled.tsx
+++ src/pages/admin/createNew/create.styled.tsx
@@ -2,8 +2,24 @@ import Styled from "styled-components";
 
 export const FormCreate = Styled.form`
 
-`
+`;
 
 export const FormControl = Styled.div`
-   
-`
+   display: flex;
+   gap: 10px;
+   margin: 20px 0;
+   padding: 10px;
+   border: 1px solid #00f0f0;
+   width: max-content;
+   border-radius: 10px;
+`;
+
+export const FormRow = Styled.div`
+   display: flex;
+   gap: 2rem;
+`;
+
+export const Container = Styled.div`
+   max-width: 80vw;
+   margin: 0 auto;
+`;
